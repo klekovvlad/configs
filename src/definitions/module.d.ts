52ea08edd65e913423b0b9f5624413d8c3dbb23f
Klekov Vladislav <klekov@reksoft.ru>
2024-02-04 13:09:32 +0300

    upd

--- src/definitions/module.d.ts
+++ src/definitions/module.d.ts
@@ -3,6 +3,16 @@ declare module '*.png' {
   export default png;
 }
 
+declare module '*.jpg' {
+  const jpg: string;
+  export default jpg;
+}
+
+declare module '*.gif' {
+  const gif: string;
+  export default gif;
+}
+
 declare module '*.svg' {
   import React = require('react');
   export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
