3a58336383354625d55a4361a7d6f66a94248f30
Klekov Vladislav <klekov@reksoft.ru>
2024-02-09 23:28:26 +0300

    Исправление иконок

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
       '@ui': path.resolve(__dirname, './src/ui'),
+      '@ui/icons': path.resolve(__dirname, './src/ui/icons'),
     },
   },
 });
